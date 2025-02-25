e0fcdf84025472cb98a30729a0fe1ef33d878a86
jo-27 <joansharon270704@gmail.com>
2025-02-25 13:47:51 +0530

    day12an

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,47 @@
-const express = require('express')
+const express = require("express");
+const mdb = require("mongoose");
+const dotenv=require('dotenv')
+const Signup=require('./models/signupSchema')
+const app = express();
+app.use(express.json())
+const PORT = 3001;
+dotenv.config()
+mdb
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MBD sucess");
+  })
+  .catch((err) => {
+    console.log("cheack you string", err);
+  });
 
-const app=express()
+app.get("/", (req, res) => {
+  res.send("<h1>welcome back<h1>");
+});
+app.get("/static", (req, res) => {
+  res.sendFile(
+    "C:\\Users\\joans\\OneDrive\\Documents\\desktop\\mern\\html_css\\index.html"
+  );
+});
 
-app.get("/",(req,res)=>{
-    res.send("<h1>welcome back<h1>")
+app.post("/signup",(req,res)=>{
+    try {
+        const {firstName,lastName,email,password,phoneNumber}=req.body
+    const newSignup=new Signup({
+        firstName:firstName,
+        lastName:lastName,
+        phoneNumber:phoneNumber,
+        password:password,
+        email:email
+    });
+    newSignup.save()
+    console.log("signup sucess")
+    res.status(201).json({message:"Signup Successfull",isSignup:true})
+        
+    } catch (error) {
+        res.status(400).json({message:"Signup UnSuccessfull",isSignup:false})
+    }
+
 })
-app.get("/static",(req,res)=>{
-    res.sendFile("C:\\Users\\joans\\OneDrive\\Documents\\desktop\\mern\\html_css\\index.html")
-})
-const PORT =3001
 
-app.listen(PORT,()=>console.log("server started successfully"))
+app.listen(PORT, () => console.log("server started successfully"));
